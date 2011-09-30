98c09aeaca519c8e588fb17be95d84a2cea60c31
Matt Raibert <mattraibert@gmail.com>
2011-09-30 00:27:08 -0500

    problem 3. With a crappy prime test

---
+++ problem3.rb
@@ -0,0 +1,17 @@
+def divides? x, y
+  y % x == 0
+end
+
+def factors n
+  (Math.sqrt(n).truncate.downto 1).select { |x| divides? x, n }
+end
+
+def prime? n
+  n != 1 && factors(n).size == 1
+end
+
+def prime_factors n
+  factors(n).select { |x| prime? x }
+end
+
+puts prime_factors 600851475143
