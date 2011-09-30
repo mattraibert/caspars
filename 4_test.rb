0d64f919ea90729636cd6a99c900e55acb756d09
Matt Raibert <mattraibert@gmail.com>
2011-09-30 11:52:14 -0500

    problem 4

--- 4_test.rb
+++ 4_test.rb
@@ -8,4 +8,9 @@ class FourTest < MiniTest::Unit::TestCase
     assert(palindromic?(11211))
     assert(!palindromic?(11238))
   end
+
+  def test_something
+    pairs = pairs 4
+    assert_equal([[4,4], [4,3], [3,4], [3,3], [4,2], [2,4], [3,2], [2,3], [2,2], [3,1], [1,3], [2,1], [1,1]], pairs)
+  end
 end
