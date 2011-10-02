aa7fe84f418e38702f9235ded7399126ee5e8c64
Matt Raibert <mattraibert@gmail.com>
2011-10-02 17:45:28 -0500

    lazy enum methods (map and select); plus series helper

---
+++ lazy_enum.rb
@@ -0,0 +1,21 @@
+module Enumerable
+  def lazy_select(&block)
+    Enumerator.new do |yielder|
+      self.each do |element|
+        yielder << element if yield(element)
+      end
+    end
+  end
+
+  def lazy_map(&block)
+    Enumerator.new do |yielder|
+      self.each do |element|
+        yielder << yield(element)
+      end
+    end
+  end
+end
+
+def series(&block)
+  (0..(1.0/0.0)).lazy_map {|x| yield x}
+end
