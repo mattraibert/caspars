8b60c1096798828a5723c077a6e92e77470daf87
Matt Raibert <mattraibert@gmail.com>
2011-10-02 17:41:22 -0500

    rename factors to small_factors because that's what it really does

--- 3_util.rb
+++ 3_util.rb
@@ -2,14 +2,18 @@ def divides? x, y
   y % x == 0
 end
 
-def factors n
+def small_factors n
   (Math.sqrt(n).truncate.downto 1).select { |x| divides? x, n }
 end
 
+def factors n
+  [n] + ((n/2).truncate.downto 1).select { |x| divides? x, n }
+end
+
 def prime? n
-  n != 1 && factors(n).size == 1
+  n != 1 && small_factors(n).size == 1
 end
 
 def prime_factors n
-  factors(n).select { |x| prime? x }
+  small_factors(n).select { |x| prime? x }
 end
--- 3_util_test.rb
+++ 3_util_test.rb
@@ -6,4 +6,17 @@ class ThreeUtilTest < MiniTest::Unit::TestCase
     assert_equal 2, [1,2,3,4].find_first { |x| x % 2 == 0 }
     assert_equal 4, [3,4].find_first { |x| x % 2 == 0 }
   end
+
+  def test_small_factors_skips_factors_above_sqrt
+    assert_equal [2,1], small_factors(6)
+    assert_equal [2,1], small_factors(14)
+  end
+
+  def test_factors_finds_all_factors
+    assert_equal [6,3,2,1], factors(6)
+    assert_equal [14,7,2,1], factors(14)
+  end
 end
+
+
+
