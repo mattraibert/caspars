03558943a148483446df8d7dd85c9114d2de3022
Matt Raibert <mattraibert@gmail.com>
2011-10-10 12:30:54 -0400

    remove parens

--- 5.rb
+++ 5.rb
@@ -13,7 +13,7 @@ pop = find_primes(20).inject(:*)
 
 first = (1..(1.0/0.0)).find_first do |x|
   the_answer = x * pop
-  (not required_factors.includes_match? { |factor| !divides?(factor, the_answer) } )
+  not required_factors.includes_match? { |factor| !divides?(factor, the_answer) }
 end
 
 puts pop * first
