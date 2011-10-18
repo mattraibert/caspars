7aef4c0cc5c8bfc07f5edf2936674c55654183c3
Matt Raibert <mattraibert@gmail.com>
2011-10-17 23:39:42 -0400

    oops broke 14. Now fixed.

--- 14.rb
+++ 14.rb
@@ -1,24 +1,4 @@
 require 'lazy_enum'
 require '14_util'
 
-start = 0
-max_chain_size = 0
-
-(1..1000000).each do |x|
-  chain_size = chain(x).size
-  if chain_size > max_chain_size
-    index = x 
-    max_chain_size = chain_size
-  end
-end
-
-puts start
-
-
-
-
-
-
-
-
-
+puts (1..1000000).map { |x| [x, chain(x).size] }.max { |one, another| one[1] <=> another[1] }[0]
