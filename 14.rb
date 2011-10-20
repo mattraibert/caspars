e3437cb14478e26f83ba0d854f1da709990e296c
Matt Raibert <mattraibert@gmail.com>
2011-10-19 21:37:57 -0400

    more cleanup 14

--- 14.rb
+++ 14.rb
@@ -1,4 +1,4 @@
 require 'lazy_enum'
 require '14_util'
 
-puts (1..1000000).map { |x| [x, chain(x).size] }.max { |one, another| one[1] <=> another[1] }[0]
+puts (1..1_000_000).map { |x| [chain(x).size, x] }.max[1]
