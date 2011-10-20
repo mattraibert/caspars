e2794dda3b2b21f15dfe7e18f9319051be69dd09
Matt Raibert <mattraibert@gmail.com>
2011-10-19 22:41:21 -0400

    a

--- 14.rb
+++ 14.rb
@@ -1,8 +1,5 @@
 require 'lazy_enum'
 require '14_util'
-require 'perf_util'
 
-perf_time "" do
-  cc = ChainCache.new
-  puts (1..1_000_000).lazy_map { |x| [cc.chain_length(x),x] }.max[1]
-end
+cc = ChainCache.new
+puts (1..1_000_000).lazy_map { |x| [cc.chain_length(x),x] }.max[1]
