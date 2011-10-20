997f9faeeab83eda4285505d1f43d8d82057442f
Matt Raibert <mattraibert@gmail.com>
2011-10-19 20:11:12 -0400

    test drive a cache for storing chain lengths; will need to integrate into calculation fcn

--- 14_util.rb
+++ 14_util.rb
@@ -7,6 +7,38 @@ def fcn n
   end
 end
 
+class ChainCache
+  def initialize
+    @cc = [nil, 1]
+  end
+  
+  def lookup_length start
+    @cc[start]
+  end
+
+  def record_length item, length
+    @cc[item] = length
+  end
+
+  def is_cached? start
+    !lookup_length(start).nil?
+  end
+
+  def first_known chain_prefix
+    chain_prefix.find { |x| is_cached? x }
+  end
+
+  def unknown_portion chain_prefix
+    index = chain_prefix.find_index { |x| is_cached? x } - 1
+    chain_prefix[0..index]
+  end
+
+  def record_lengths unknown_prefix, known_item
+    known_length = lookup_length(known_item)
+    unknown_prefix.tails.each { |x| @cc[x[0]] = x.size + known_length }
+  end
+end
+
 def chain n
   chain = [n]
   while(n > 1)
--- 14_util_test.rb
+++ 14_util_test.rb
@@ -2,7 +2,41 @@ require 'minitest/autorun'
 require '14_util'
 
 class FourteenUtilTest < MiniTest::Unit::TestCase
+  def setup
+    @cc = ChainCache.new
+  end
+
   def test_chain
     assert_equal [13,40,20,10,5,16,8,4,2,1], chain(13)
+  end
+
+  def test_basics
+    @cc.record_length 2,2
+    assert_equal 1, @cc.lookup_length(1)
+    assert_equal 2, @cc.lookup_length(2)
+    assert @cc.is_cached? 1
+    assert @cc.is_cached? 2
+    assert !@cc.is_cached?(3)
+  end
+
+  def test_first_known
+    @cc.record_length 4,3
+    @cc.record_length 2,2
+    assert_equal 4, @cc.first_known([16,8,4,2])
+  end
+
+  def test_unknown_portion
+    @cc.record_length 4,3
+    @cc.record_length 2,2
+    assert_equal [16,8], @cc.unknown_portion([16,8,4,2])
+  end
+
+  def test_record_chain_from_prefix
+    @cc.record_lengths [5,16,8,4,2], 1
+    assert_equal 2, @cc.lookup_length(2)
+    assert_equal 3, @cc.lookup_length(4)
+    assert_equal 4, @cc.lookup_length(8)
+    assert_equal 5, @cc.lookup_length(16)
+    assert_equal 6, @cc.lookup_length(5)
   end
 end
