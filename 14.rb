46e3ba517f54c6620e37caf7751a0316e00b9560
Matt Raibert <mattraibert@gmail.com>
2011-10-19 22:40:07 -0400

    increase to a million

--- 14.rb
+++ 14.rb
@@ -1,5 +1,8 @@
 require 'lazy_enum'
 require '14_util'
+require 'perf_util'
 
-cc = ChainCache.new
-puts (1..50_000).lazy_map { |x| p x; [cc.chain_length(x),x] }.max[1]
+perf_time "" do
+  cc = ChainCache.new
+  puts (1..1_000_000).lazy_map { |x| [cc.chain_length(x),x] }.max[1]
+end
