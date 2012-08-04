606e682df5da2c0ba10a49ae8af92f3e83993fc0
Matt Raibert <mattraibert@gmail.com>
2012-08-04 09:45:23 -0400

    quickly discover amicable numbers

--- 21.rb
+++ 21.rb
@@ -1,5 +1,9 @@
 require './prime_util'
 
-pairs = (0..10_000).map { |x| proper_factors(x).inject(:+) }.each_with_index.map {|n, i|[i, n]}
+def sum_fact(n)
+  proper_factors(n).inject(:+) || 0
+end
 
-puts pairs.select {|x| pairs.include? [x[1], x[0]]}.reject {|x| x[0] == x[1] }.flatten.uniq.inject(:+)
+pairs = (0..10_000).map { |x| [x, sum_fact(x)] }
+
+puts pairs.select { |x| pairs[x[1]] == [x[1], x[0]] }.reject { |x| x[0] == x[1] }.flatten.uniq.inject(:+)
