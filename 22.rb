bc28012c4e603a0903b7cfc847c3afc2213afff2
Matt Raibert <mattraibert@gmail.com>
2012-08-03 21:40:22 -0400

    solve 22

---
+++ 22.rb
@@ -0,0 +1,11 @@
+def value(char)
+  ('A'..'Z').to_a.find_index(char) + 1
+end
+
+def value_str(str)
+  str.each_char.map { |x| value(x) }.inject(:+)
+end
+
+names = IO.read("./names.txt").gsub(/"/, '').split(/,/).sort
+
+puts names.map {|name| value_str name }.each_with_index.map { |name, i| name * (i + 1) }.inject(:+)
