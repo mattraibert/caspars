9601739c00042a24804dcb51c194d539ea0e2f44
Matt Raibert <mattraibert@gmail.com>
2012-08-08 13:55:48 -0400

    find the recurring cycles

--- 26_util.rb
+++ 26_util.rb
@@ -1,15 +1,18 @@
-def digits(rat, how_many)
+def recurring_cycle(rat)
   n = rat.numerator
   d = rat.denominator
   q, r = n.divmod d
-  digits = []
-  how_many.times do
-    digits << [q,r]
+  remainders = []
+  until remainders.include? r
+    remainders << r
     q, r = (r * 10).divmod d
   end
-  digits
+  remainders << r
+  remainders
+  return [0] if remainders.last == 0
+  remainders[remainders.index(remainders.last)..-1]
 end
 
 def cycle_length rat
-  digits(rat, 100)
+  recurring_cycle(rat).size - 1
 end
--- 26_util_test.rb
+++ 26_util_test.rb
@@ -3,10 +3,9 @@ require './26_util'
 
 class TwentySixUtilTest < MiniTest::Unit::TestCase
   def test_cycle_length
-    (1..1000).each do |n|
-      puts digits(Rational(1, n), 100).inspect
-    end
-    assert_equal(6, cycle_length(Rational(1, 99)))
-    assert_equal(1, cycle_length(Rational(1, 3)))
+    assert_equal(1, cycle_length(Rational(1, 3)), recurring_cycle(Rational(1, 3)))
+    assert_equal(6, cycle_length(Rational(1, 7)), recurring_cycle(Rational(1, 7)))
+    assert_equal(0, cycle_length(Rational(1, 2)), recurring_cycle(Rational(1, 2)))
+    assert_equal(1, cycle_length(Rational(1, 6)), recurring_cycle(Rational(1, 6)))
   end
 end
