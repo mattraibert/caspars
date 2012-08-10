a8a9f155c08035820947ac1f77fa1effe4827310
Matt Raibert <mattraibert@gmail.com>
2012-08-10 10:52:59 -0400

    iterative version of permute

--- 24_util.rb
+++ 24_util.rb
@@ -1,6 +1,17 @@
+def permutation(array, i)
+  available_digits = array.dup
+  ((available_digits.size - 1).downto 0).map do |x|
+    f = (i / factorial(x)) % available_digits.size
+    available_digits.delete(available_digits[f])
+  end
+end
+
 def permute array
-  return [array] if array.size == 1
-  array.map do |element|
-    permute(array - [element]).map {|sub| [element] + sub }
-  end.flatten(1)
+  factorial(array.size).times.map do |i|
+    permutation(array, i)
+  end
+end
+
+def factorial(n)
+  (1..n).inject(:*) || 1
 end
--- 24_util_test.rb
+++ 24_util_test.rb
@@ -7,4 +7,11 @@ class TwentyFourUtilTest < MiniTest::Unit::TestCase
     assert_equal([[0, 1], [1, 0]], permute([0, 1]))
     assert_equal([[0, 1, 2], [0, 2, 1], [1, 0, 2], [1, 2, 0], [2, 0, 1], [2, 1, 0]], permute([0, 1, 2]))
   end
+
+  def test_factorial
+    assert_equal(1, factorial(1))
+    assert_equal(2, factorial(2))
+    assert_equal(6, factorial(3))
+    assert_equal(24, factorial(4))
+  end
 end
