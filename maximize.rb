5e1a66e950246fdfe553e5016cb673b724779668
Matt Raibert <mattraibert@gmail.com>
2012-08-11 10:01:03 -0400

    make maximize fcn use lazy map

--- maximize.rb
+++ maximize.rb
@@ -1,4 +1,6 @@
+require './lazy_enum'
+
 #find the input in the range which maximizes the result of the block
 def maximize(range = 1..1_000_000)
-  (range).map { |x| [yield(x), x] }.max[1]
+  (range).lazy_map { |x| [yield(x), x] }.max[1]
 end
