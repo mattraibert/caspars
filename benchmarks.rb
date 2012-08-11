3fcad47faa48f071c7f61e109ebedff4be44bd73
Matt Raibert <mattraibert@gmail.com>
2012-08-11 00:27:39 -0400

    switch benchmarks back to compare seconds instead of percent

--- benchmarks.rb
+++ benchmarks.rb
@@ -12,16 +12,15 @@ prev_time = eval File.read("performance.rb")
 IO.write("performance.rb", performance.to_s.gsub(/,/, ",\n"))
 
 comparisons = performance.keys.map do |k|
-  previous_run = (prev_time[k] || 0)
-  delta = (previous_run - performance[k])
-  delta = (100 * delta / previous_run).round(2)
+  #TODO use percentage change
+  delta = ((prev_time[k] || 0) - performance[k]).round(2)
   case delta
-    when -10..10
-      "#{k}: not much change (#{delta}%)"
-    when (-1.0/0.0)..-10
-      "#{k}: got slower by #{delta}%"
-    when 10..(1.0/0.0)
-      "#{k}: got faster by #{delta}%"
+    when -0.1..0.1
+      "#{k}: not much change (#{delta})"
+    when (-1.0/0.0)..-0.1
+      "#{k}: got slower by #{delta}"
+    when 0.1..(1.0/0.0)
+      "#{k}: got faster by #{delta}"
     else
       "#{k} doesn't seem to have a score"
   end
