d66cb13773b9d93549fb9aa04d2ee56a7ae42e33
Matt Raibert <mattraibert@gmail.com>
2012-08-12 01:31:46 -0400

    improve readability of change method

--- 31_util.rb
+++ 31_util.rb
@@ -1,9 +1,7 @@
-def make_change(prefix = [], this_much, from)
-  this_coin = from[0]
-  max_coins = this_much/this_coin
-  return [prefix + [max_coins]] if from.size == 1
-  max_coins.downto(0).map do |num_coins|
-    remaining_value = this_much - num_coins*this_coin
-    make_change(prefix + [num_coins], remaining_value, from[1..-1])
+def make_change(value, coins)
+  return [[value/coins.first]] if coins.size == 1
+
+  (value/coins.first).downto(0).map do |n|
+    make_change(value - n*coins.first, coins[1..-1]).map { |x| [n] + x }
   end.flatten(1)
 end
