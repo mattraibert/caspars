ca05a18d7bac5ba95d4892a7361bb19355af4a9e
Matt J Raibert <mattraibert@positiondev.com>
2022-08-28 15:12:35 -0400

    More info in benchmark comparisons

--- benchmarks.rb
+++ benchmarks.rb
@@ -11,16 +11,26 @@ prev_time = eval File.read("performance.rb")
 
 IO.write("performance.rb", performance.to_s.gsub(/,/, ",\n"))
 
+def percent_change(prev, current)
+  100 * ((current - prev)/prev.abs)
+end
+
 comparisons = performance.keys.map do |k|
-  #TODO use percentage change
-  delta = ((prev_time[k] || 0) - performance[k]).round(2)
-  case delta
-    when -0.1..0.1
-      "#{k}: not much change (#{delta})"
-    when (-1.0/0.0)..-0.1
-      "#{k}: got slower by #{delta}"
-    when 0.1..(1.0/0.0)
-      "#{k}: got faster by #{delta}"
+
+  prev = prev_time[k] || 0
+  current = performance[k]
+  percent_delta = percent_change((prev) , current).round(1)
+  time_delta = (current - prev).round(3)
+
+  change_str = "#{percent_delta}% (#{time_delta} seconds) was: #{prev.round(3)}, now: #{current.round(3)}"
+
+  case percent_delta
+    when -10..10
+      "#{k}: not much change (#{change_str})"
+    when (-1.0/0.0)..-10
+      "#{k}: got faster by #{change_str}"
+    when 10..(1.0/0.0)
+      "#{k}: got slower by #{change_str}"
     else
       "#{k} doesn't seem to have a score"
   end
