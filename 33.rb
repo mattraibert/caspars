465a15a87a1f5387e29128462cf967add1f9804e
Matt J Raibert <mattraibert@positiondev.com>
2024-11-04 09:38:19 -0500

    problem 33

---
+++ 33.rb
@@ -0,0 +1,16 @@
+def n33
+  cancellers = []
+  (1..9).map do |cancel|
+    (1..9).map do |n|
+      (1..9).map do |d|
+        numerator = n * 10 + cancel
+        denominator = cancel * 10 + d
+        if n.to_f / d == numerator.to_f / denominator && numerator != denominator
+          cancellers << Rational(numerator, denominator)
+        end
+      end
+    end
+  end
+
+  cancellers.inject(:*).denominator
+end
