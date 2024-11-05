f92246335586c36356420adf44207ba12d150342
Matt J Raibert <mattraibert@positiondev.com>
2024-11-05 10:12:59 -0500

    optimize factorial with caching

--- 24_util.rb
+++ 24_util.rb
@@ -1,3 +1,5 @@
+require 'five_leaves/lazy_enum'
+
 def permutation(array, i)
   available_digits = array.dup
   ((available_digits.size - 1).downto 0).map do |x|
@@ -13,5 +15,13 @@ def permute array
 end
 
 def factorial(n)
-  (1..n).inject(:*) || 1
+  @factorials ||= { 0 => 1, 1 => 1 }
+  if @factorials[n]
+    return @factorials[n]
+  end
+  i, f = n.downto(1).lazy_map { |i| [i, @factorials[i]] }.find { |i, f| f }
+
+  f2 = ((i + 1)..n).inject(:*)
+  fact = f * f2
+  @factorials[n] = fact
 end
