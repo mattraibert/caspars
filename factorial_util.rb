56c17fcf4d6c32c5d27c4c741777b0a3a5cde552
Matt J Raibert <mattraibert@positiondev.com>
2024-11-09 14:49:05 -0500

    optimize 34 with caching

--- factorial_util.rb
+++ factorial_util.rb
@@ -1,12 +1,11 @@
 def factorial(n)
   @factorials ||= { 0 => 1, 1 => 1 }
-  if @factorials[n]
-    return @factorials[n]
-  end
+  cached = @factorials[n]
+  return cached if cached
+
   i, f = n.downto(1).lazy_map { |i| [i, @factorials[i]] }.find { |i, f| f }
 
   f2 = ((i + 1)..n).inject(:*)
   fact = f * f2
-  puts "computed #{i + 1} to #{n}"
   @factorials[n] = fact
 end
--- solutions/34.rb
+++ solutions/34.rb
@@ -1,16 +1,27 @@
 require './factorial_util'
 
 def digit_factorials(n)
-  n.digits.lazy_map do |d|
+  n.digits.map do |d|
     factorial(d)
   end
 end
 
+def dfs(n)
+  return 0 if n == 0
+  @dfs ||= []
+  cached = @dfs[n]
+  return cached if cached
+
+  @dfs[n] = dfs(n.div 10) + factorial(n % 10)
+end
+
 def curious?(n)
-  return false if [1, 2].include? n
-  digit_factorials(n).sum == n
+  return false if n == 1 or n == 2
+  dfs(n) == n
 end
 
 def n34
-  1_0000_000.times.map.select { |n| curious? n }.sum
+  1_0000_000.times.map.select do |n|
+    curious? n
+  end.sum
 end
