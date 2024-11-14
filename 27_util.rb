e59da4153e39052814e5995cdf99b5c2c6cdef6f
Matt J Raibert <mattraibert@positiondev.com>
2024-11-14 06:24:07 -0500

    wip

--- 27_util.rb
+++ 27_util.rb
@@ -1,11 +1,23 @@
-require 'five_leaves/lazy_enum'
-require 'five_leaves/range_util'
+# require 'five_leaves/lazy_enum'
+# require 'five_leaves/range_util'
 require 'prime'
 
+def pow2(n)
+  @cache_pow2 ||= []
+  cached = @cache_pow2[n]
+  if cached
+    cached
+  else
+    @cache_pow2[n] = n ** 2
+  end
+end
+
 def quadratic(a, b, c, n)
-  a * n**2 + b * n + c
+  a * pow2() + b * n + c
 end
 
 def how_many_consecutive_primes?(b, c)
-  FiveLeaves.find { |n| !(quadratic(1, b, c, n)).prime? }
+  (0..(1.0 / 0.0)).find { |n| !(quadratic(1, b, c, n)).prime? }
+
+  # FiveLeaves.find { |n| !(quadratic(1, b, c, n)).prime? }
 end
--- solutions/27.rb
+++ solutions/27.rb
@@ -1,8 +1,16 @@
 require './27_util'
 require 'five_leaves/maximize'
+def maximize(range = 1..1_000_000)
+  (range).lazy_map do |x|
+    [yield(x), x]
+  end.max[1]
+end
 
 def n27
-  b, c = FiveLeaves.maximize((-999..999).pair_with(1..999)) { |b, c| how_many_consecutive_primes?(b, c) }
+  pairs = (-999..999).pair_with(1..999)
+  b, c = maximize(pairs) do |b, c|
+    how_many_consecutive_primes?(b, c)
+  end
 
   b * c
 end
